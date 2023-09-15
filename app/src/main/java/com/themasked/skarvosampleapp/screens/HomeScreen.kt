397ad3cf38de464f952d3a34c8525ddd2062a12f
Mohd Raghib <raghib.khan76@gmail.com>
2023-09-15 20:50:02 +0530

    feat: make bottom sheet draggable and visible all the time

--- app/src/main/java/com/themasked/skarvosampleapp/screens/HomeScreen.kt
+++ app/src/main/java/com/themasked/skarvosampleapp/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.themasked.skarvosampleapp.screens
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -7,15 +8,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,8 +30,21 @@ import androidx.navigation.NavController
 import com.themasked.skarvosampleapp.R
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
+
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    //for making the bottom sheet reach half of the device's height
+    val density = LocalDensity.current
+    val screenHeight = with(density) {(LocalConfiguration.current.screenHeightDp).dp}
+    val midY = screenHeight / 2
+
+    val bottomSheetScaffoldState = scaffoldState.bottomSheetState.targetValue
+    Log.d("Home Screen", "$bottomSheetScaffoldState")
+
     Column {
         Row(
             Modifier
@@ -56,6 +76,25 @@ fun HomeScreen(navController: NavController) {
             )
         }
 
-        StockList()
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetContent = {
+                Column(
+                    modifier = Modifier
+                        .height(midY)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Skarvo Sample App",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+            },
+            sheetPeekHeight = 72.dp,
+            content = {
+                StockList()
+            },
+        )
     }
 }
--- app/src/main/java/com/themasked/skarvosampleapp/screens/StockList.kt
+++ app/src/main/java/com/themasked/skarvosampleapp/screens/StockList.kt
@@ -4,36 +4,33 @@ import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.DismissState
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDismissState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.themasked.skarvosampleapp.R
 import com.themasked.skarvosampleapp.models.StockItemModel
 import com.themasked.skarvosampleapp.viewmodel.StockItemsViewModel
 
@@ -49,20 +46,20 @@ fun StockList() {
             items(items = value, key = { stock -> stock.id!! }) { stockItem ->
                 val dismissState = rememberDismissState(
                     initialValue = DismissValue.Default,
-                    positionalThreshold = {swipeActivationFloat -> swipeActivationFloat/3}
+                    positionalThreshold = { swipeActivationFloat -> swipeActivationFloat / 3 }
                 )
                 SwipeToDismiss(
                     modifier = Modifier.animateItemPlacement(),
                     state = dismissState,
                     directions = setOf(DismissDirection.EndToStart),
                     background = {
-                      val color by animateColorAsState(
-                          when (dismissState.targetValue) {
-                              DismissValue.Default -> Color.Transparent
-                              DismissValue.DismissedToEnd -> Color.Transparent
-                              DismissValue.DismissedToStart -> Color.Red
-                          }, label = ""
-                      )
+                        val color by animateColorAsState(
+                            when (dismissState.targetValue) {
+                                DismissValue.Default -> Color.Transparent
+                                DismissValue.DismissedToEnd -> Color.Transparent
+                                DismissValue.DismissedToStart -> Color.Red
+                            }, label = ""
+                        )
                         Box(
                             modifier = Modifier
                                 .fillMaxSize()
@@ -84,12 +81,13 @@ fun StockList() {
                     },
                 )
                 if (dismissState.isDismissed(DismissDirection.EndToStart)) {
-                    Log.d("Dismissed",   "Dismissed id: ${stockItem.id}")
+                    Log.d("Dismissed", "Dismissed id: ${stockItem.id}")
                     stockItem.id?.let {
                         stockItemsViewModel.deleteStock(it)
                     }
                 }
             }
         }
-    })
+    }
+    )
 }
